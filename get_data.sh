d7147c8c435d3efe35b254cf3b6f4b02e27eec9e
EC2 Default User <ec2-user@ip-172-31-54-149.ec2.internal>
2016-10-31 02:55:28 +0000

    .

---
+++ get_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -v
+cd data
+if [ ! -f HIGGS.csv ]; then
+	wget -nc -nH https://s3.amazonaws.com/xgboosttestdata/HIGGS.csv.gz
+	gzip -d HIGGS.csv.gz HIGGS.csv
+	python higgs2libsvm.py
+fi
+if [ ! -f set1.test.txt ]; then
+	wget -nc -nH https://s3.amazonaws.com/xgboosttestdata/set1.test.txt
+	wget -nc -nH https://s3.amazonaws.com/xgboosttestdata/set1.train.txt 
+	python yahoo2libsvm.py
+fi
